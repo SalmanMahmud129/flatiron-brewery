a5af55d1fd99df585df5f15464898b24c1895863
Alex Ju <aju217@gmail.com>
2022-10-25 17:47:38 -0400

    renamed header to navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,10 +1,10 @@
 import React from "react"
 import {NavLink} from 'react-router-dom'
 
-function Header(){ 
+function NavBar(){ 
     return (
         <nav>
-            <NavLink to='/'>Home</NavLink>
+            <NavLink exact to='/'>Home</NavLink>
             <NavLink to='/beers'>Beers</NavLink>
             <NavLink to='/addbeer'>Add Beer</NavLink>
         </nav>
@@ -14,4 +14,4 @@ function Header(){
     )
 }
 
-export default Header
+export default NavBar
