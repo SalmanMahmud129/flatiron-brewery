4c6765f1e4cf5180e26317565304cc348ba2b1d8
Salman Mahmud <salman_mahmud@hotmail.com>
2022-10-27 12:20:35 -0400

    where we left off in extra help. Data is duplicating when trying to post from external to our local json. Mapped data out to only get data that relates to form. Need to figure out duplication issue

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Routes, Route} from 'react-router-dom'
 import NavBar from './NavBar'
 import BeerPage from './BeerPage'
@@ -8,13 +8,63 @@ import './App.css';
 
 function App() {
   const [page, setPage] = useState('/')
+  const [beerData, setBeerData] = useState([]);
+  
 
+
+
+  useEffect(() => {
+    fetch('https://api.punkapi.com/v2/beers/')
+    .then(resp => resp.json())
+    .then(data => {
+        // setBeerData(data)
+        data.forEach(beer => {
+          postDataLocally(beer)
+        })
+
+    })
+},[])
+
+
+const localAPI = "http://localhost:3001/beers"
+function postDataLocally(beerData){
+  const beerDataBody = {
+    name: beerData.name,
+    tagline: beerData.tagline,
+    description: beerData.description,
+    image_url: beerData.image_url,
+    first_brewed: beerData.first_brewed,
+    abv: beerData.abv,
+    food_pairing: beerData.food_pairing
+  }
+    fetch(localAPI, {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+    },
+    body : JSON.stringify(beerDataBody)
+    })
+    .then(resp => resp.json())
+    .then(data =>{
+      console.log(data)
+      // setBeerData([...beerData, data])
+    })
+    
+}
+
+
+    function displayAddedBeer(newBeer){
+        setBeerData([...beerData, newBeer])
+    }
+
+    console.log(beerData)
   return (
     <div>
       <NavBar onChangePage={setPage} />
       <Routes>
-        <Route path='/beers' element={<BeerPage />}/>
-        <Route path='/addbeer' element={<AddBeerForm />}/>
+        <Route path='/beers' element={<BeerPage beerData={beerData} />}/>
+        <Route path='/addbeer' element={<AddBeerForm displayAddedBeer={displayAddedBeer}/>}/>
         <Route exact path='/' element={<Home />} />
       </Routes>
     </div>
--- src/Components/BeerPage.js
+++ src/Components/BeerPage.js
@@ -2,41 +2,16 @@ import React, {useState, useEffect} from "react"
 import Search from "./Search";
 import BeerCollection from "./BeerCollection";
 
-function BeerPage(){
+function BeerPage({beerData}){
 
-    const [beerData, setBeerData] = useState([]);
+    
     const [search, setSearch] = useState('')
 
-    useEffect(() => {
-        fetch('https://api.punkapi.com/v2/beers/')
-        .then(resp => resp.json())
-        .then(data => {
-            setBeerData(data)
-            postDataLocally(data)
-        })
-    },[])
-
-
-    const localAPI = "http://localhost:3001/beers"
-    function postDataLocally(beerData){
-        fetch(localAPI, {
-            method: "POST",
-            headers: {
-                "Content-Type" : "application/json",
-                "Accept" : "application/json"
-        },
-        body : JSON.stringify(beerData)
-        })
-        .then(resp => resp.json())
-        
-    }
+   
 
     // console.log(beerData)
 
-    //this function will be passed to addbeerform' handle submit within the second then of the post request. Make sure to pass it down as a prop on line 26
-    // function displayAddedBeer(newBeer){
-    //     setBeerData([...beerData, newBeer])
-    // }
+    
     
 
     
