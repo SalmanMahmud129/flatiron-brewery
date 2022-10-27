da1f28c190016ec544384e8940beb61ae3e42753
Salman Mahmud <salman_mahmud@hotmail.com>
2022-10-27 16:38:14 -0400

    all functionality done

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {Routes, Route} from 'react-router-dom'
 import NavBar from './NavBar'
 import BeerPage from './BeerPage'
@@ -8,14 +8,28 @@ import Home from './Home'
 
 function App() {
   const [page, setPage] = useState('/')
+  const [beerData, setBeerData] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3001/beers')
+    .then(resp => resp.json())
+    .then(data => {
+        setBeerData(data)
+        // postDataLocally(data)
+    })
+},[])
+
+function displayAddedBeer(newBeer){
+  setBeerData([...beerData, newBeer])
+}
 
   return (
     <div>
       <NavBar onChangePage={setPage} />
       <Routes>
-        <Route path='/beers' element={<BeerPage />}/>
+        <Route path='/beers' element={<BeerPage beerData={beerData} />}/>
         <Route path='/beers/:id' element={<BeerDetail/>}/>
-        <Route path='/addbeer' element={<AddBeerForm />}/>
+        <Route path='/addbeer' element={<AddBeerForm displayAddedBeer={displayAddedBeer}/>}/>
         <Route path='/' element={<Home />} />
       </Routes>
     </div>
--- src/Components/BeerDetail.js
+++ src/Components/BeerDetail.js
@@ -1,48 +1,61 @@
-import React, {useEffect, useState} from 'react'
-import {useParams} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import { Item } from 'semantic-ui-react'
 
-function BeerDetail(){
+
+function BeerDetail() {
     const [beer, setBeer] = useState([])
     const params = useParams()
-    
-    useEffect(()=> {
-        fetch(`https://api.punkapi.com/v2/beers/${params.id}`)
-        .then(resp=>resp.json())
-        .then(data => setBeer(data[0]))
-    },[params.id])
 
-    console.log(beer.food_pairing)
+    useEffect(() => {
+        fetch(`http://localhost:3001/beers/${params.id}`)
+            .then(resp => resp.json())
+            .then(data => {
+                // console.log(data)
+                setBeer(data)
+
+            })
+    }, [params.id])
+
+    // let isThereFoodPairings = beer.food_pairing ? 
+    // beer.food_pairing.map(food =>{
+    //     console.log(food)
+    // }) : "none"
+
+    // console.log(isThereFoodPairings)
+    // console.log(beer.food_pairing)
+
+
     return (
         <Item.Group className="beerDetail">
             <Item>
-             <Item.Image size='small' src={beer.image_url} />
-  
-            <Item.Content>
-                <Item.Header>{beer.name}</Item.Header>
+                <Item.Image size='small' src={beer.image_url} />
+
+                <Item.Content>
+                    <Item.Header>{beer.name}</Item.Header>
                     <Item.Meta>
                         <span>{beer.tagline}</span>
                     </Item.Meta>
-                <Item.Description>
-                    <span>Brewed: {beer.first_brewed}</span>
-                    <br></br>
-                    <span>abv: {beer.abv}</span>
-                    <br></br>
-                    <br></br>
-                    <strong>{beer.description}</strong>
-                    <ul>
-                        <strong>Food Pairings:</strong>
-                        <li>{beer.food_pairing}</li>                    
-                    </ul>
-                </Item.Description>
-            </Item.Content>
+                    <Item.Description>
+                        <span>Brewed: {beer.first_brewed}</span>
+                        <br></br>
+                        <span>abv: {beer.abv}</span>
+                        <br></br>
+                        <br></br>
+                        <strong>{beer.description}</strong>
+                        <ul>
+                            <strong>Food Pairings:</strong>
+                            {beer.food_pairing ? beer.food_pairing.map(food => <li key={food}>{food}</li>) : null}
+                        </ul>
+                    </Item.Description>
+                </Item.Content>
             </Item>
         </Item.Group>
-    // <>
-    //  <h1>{beer.name}</h1>
-    //  <h2>{beer.description}</h2>
-    //  <h2>{beer.tagline}</h2>
-    //  </>
+        // <>
+        //  <h1>{beer.name}</h1>
+        //  <h2>{beer.description}</h2>
+        //  <h2>{beer.tagline}</h2>
+        //  </>
     )
 }
 
