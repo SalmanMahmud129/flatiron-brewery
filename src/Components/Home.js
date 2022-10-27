bed264d1e81dee6b018690659d6713617303ab77
Alex Ju <aju217@gmail.com>
2022-10-27 17:35:20 -0400

    added background image

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,9 +8,9 @@ function Home(){
             <h1>
                 Welcome to Flatiron Brewery!
             </h1>
-            <img src='https://images.getbento.com/accounts/47c6a86449547faed082095e82035fa4/media/images/3094Screenshot_2022-03-23_5.32.08_PM.png?w=1200&fit=max&auto=compress,format'
+            {/* <img src='https://images.getbento.com/accounts/47c6a86449547faed082095e82035fa4/media/images/3094Screenshot_2022-03-23_5.32.08_PM.png'
                  alt='Flatiron Brewery'
-            />
+            /> */}
         </div>
     )
 }
